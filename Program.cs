aa82e06016a57d11c18b502dad48d4f3a5359681
huseyinkarpuzcu1 <huseyinkarpuzcu371@gmail.com>
2022-07-16 16:16:02 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Mydictionary<string> dictionary = new Mydictionary<string>();
+            dictionary.Add("Engin");
+
+
+            foreach (var item in dictionary.Items)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
